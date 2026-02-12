6f14fe1674282929beb334d51828ab71e8414c42
Kristin S <77208091+gokinisi@users.noreply.github.com>
2026-02-11 20:42:29 -0500

    Add schools data and create schoolList array

---
+++ js/schools.js
@@ -0,0 +1,665 @@
+export const schools = `
+University or College Name
+Abilene Christian University
+Academy of Art University
+Adelphi University
+Aims Community College
+Alabama A & M University
+Alabama State University
+Albany State University
+American InterContinental University System
+American University
+Ana G. Mendez University
+Anderson University
+Angelo State University
+Antelope Valley Community College District
+Appalachian State University
+Arapahoe Community College
+Arkansas State University
+Arkansas Tech University
+Ashford University
+Auburn University
+Auburn University at Montgomery
+Augusta University
+Aurora University
+Austin Peay State University
+Baker College
+Ball State University
+Barnard College
+Baylor University
+Bellevue College
+Bellevue University
+Belmont University
+Bemidji State University
+Bentley University
+Berklee College of Music
+Beth Medrash Govoha
+Binghamton University
+Biola University
+Bismarck State College
+Black Hills State University
+Boise State University
+Boston College
+Boston University
+Bowie State University
+Bowling Green State University-Main Campus
+Bradley University
+Brandeis University
+Brazosport College
+Bridgewater State University
+Brown University
+Bryant & Stratton College-Online
+Bryant University
+Bucknell University
+Butler University
+California Baptist University
+California Polytechnic State University-San Luis Obispo
+California State Polytechnic University-Humboldt
+California State Polytechnic University-Pomona
+California State University-Bakersfield
+California State University-Channel Islands
+California State University-Chico
+California State University-Dominguez Hills
+California State University-East Bay
+California State University-Fresno
+California State University-Los Angeles
+California State University-Monterey Bay
+California State University-Sacramento
+California State University-San Bernardino
+California State University-San Marcos
+California State University-Stanislaus
+Calvin University
+Cameron University
+Campbellsville University
+Capella University
+Carnegie Mellon University
+Case Western Reserve University
+Cedarville University
+Central Connecticut State University
+Central Methodist University
+Central Michigan University
+Central Ohio Technical College
+Central Washington University
+Chamberlain University-Illinois
+Chapman University
+Charleston Southern University
+Chemeketa Community College
+Christopher Newport University
+Cincinnati State Technical and Community College
+Clark Atlanta University
+Clark College
+Clark State College
+Clayton State University
+Clemson University
+Cleveland State University
+Clover Park Technical College
+Coastal Carolina University
+Colgate University
+College of Central Florida
+College of Charleston
+College of Coastal Georgia
+College of Southern Idaho
+College of Southern Nevada
+College of Staten Island CUNY
+College of the Holy Cross
+College of the Mainland
+Colorado Christian University
+Colorado Mesa University
+Colorado Mountain College
+Colorado School of Mines
+Colorado State University Global
+Colorado State University Pueblo
+Colorado State University-Fort Collins
+Colorado Technical University-Colorado Springs
+Columbia Basin College
+Columbia College
+Columbia College Chicago
+Columbia Southern University
+Columbia University in the City of New York
+Columbus State University
+Commonwealth University of Pennsylvania
+Community College of Denver
+Concordia University-Nebraska
+Concordia University-Saint Paul
+Cornell University
+Creighton University
+Culinary Institute of America
+CUNY Bernard M Baruch College
+CUNY Brooklyn College
+CUNY City College
+CUNY Graduate School and University Center
+CUNY Hunter College
+CUNY John Jay College of Criminal Justice
+CUNY Lehman College
+CUNY Medgar Evers College
+CUNY New York City College of Technology
+CUNY Queens College
+CUNY York College
+Cypress College
+Dakota State University
+Dalton State College
+Dartmouth College
+Davenport University
+Daytona State College
+Del Mar College
+Delaware State University
+Delaware Technical Community College-Terry
+DePaul University
+DeVry University-Illinois
+Drexel University
+Duke University
+Duquesne University
+East Carolina University
+East Stroudsburg University of Pennsylvania
+East Tennessee State University
+East Texas A&M University
+Eastern Connecticut State University
+Eastern Florida State College
+Eastern Illinois University
+Eastern Kentucky University
+Eastern Michigan University
+Eastern New Mexico University-Main Campus
+Eastern Washington University
+ECPI University
+Edmonds College
+Elmhurst University
+Elon University
+Embry-Riddle Aeronautical University-Daytona Beach
+Embry-Riddle Aeronautical University-Prescott
+Embry-Riddle Aeronautical University-Worldwide
+Emerson College
+Emory University
+Empire State University
+Endicott College
+Ensign College
+Everett Community College
+Excelsior University
+Fairfield University
+Fairleigh Dickinson University-Metropolitan Campus
+Fairmont State University
+Farmingdale State College
+Fashion Institute of Technology
+Fayetteville State University
+Ferris State University
+Fitchburg State University
+Florida Agricultural and Mechanical University
+Florida Atlantic University
+Florida Gulf Coast University
+Florida Institute of Technology
+Florida SouthWestern State College
+Florida State College at Jacksonville
+Foothill College
+Fordham University
+Fort Hays State University
+Fort Lewis College
+Francis Marion University
+Franciscan University of Steubenville
+Franklin University
+Front Range Community College
+Frostburg State University
+Full Sail University
+Galen College of Nursing-Louisville
+GateWay Community College
+George Fox University
+George Mason University
+George Washington University
+Georgetown University
+Georgia College & State University
+Georgia Gwinnett College
+Georgia Highlands College
+Georgia Institute of Technology-Main Campus
+Georgia Military College
+Georgia Southern University
+Georgia Southwestern State University
+Georgia State University
+Glendale Community College
+Gonzaga University
+Gordon State College
+Grambling State University
+Grand Valley State University
+Grayson College
+Great Basin College
+Green River College
+Greenville Technical College
+Gulf Coast State College
+Gurnick Academy of Medical Arts
+Hampton University
+Harding University
+Harvard University
+Hawaii Pacific University
+Henry Ford College
+High Point University
+Highline College
+Hillsborough Community College
+Hofstra University
+Hope College
+Howard University
+Idaho State University
+Illinois Institute of Technology
+Illinois State University
+Indian River State College
+Indiana State University
+Indiana University of Pennsylvania-Main Campus
+Indiana University-Indianapolis
+Indiana University-South Bend
+Indiana University-Southeast
+Indiana Wesleyan University-National & Global
+Inter American University of Puerto Rico-Bayamon
+Iona University
+Iowa State University
+Ithaca College
+Jackson College
+Jackson State University
+Jacksonville State University
+James A. Rhodes State College
+James Madison University
+Johns Hopkins University
+Johnson & Wales University-Providence
+Kansas State University
+Kean University
+Keiser University-Ft Lauderdale
+Kent State University at Kent
+Kent State University at Stark
+Kutztown University of Pennsylvania
+Lake Washington Institute of Technology
+Lake-Sumter State College
+Lamar University
+Lander University
+Laramie County Community College
+Laredo College
+Lee University
+Lehigh University
+LeTourneau University
+Lewis University
+Lewis-Clark State College
+Lindenwood University
+Lipscomb University
+Long Island University
+Longwood University
+Lorain County Community College
+Los Angeles Film School
+Louisiana State University-Alexandria
+Louisiana Tech University
+Loyola Marymount University
+Loyola University Chicago
+Loyola University Maryland
+Loyola University New Orleans
+Marion Technical College
+Marist College
+Marquette University
+Marshall University
+Maryville University of Saint Louis
+Massachusetts Institute of Technology
+McMurry University
+McNeese State University
+MCPHS University
+Mercer University
+Mercy University
+Merrimack College
+Mesa Community College
+Metropolitan State University
+Metropolitan State University of Denver
+Miami University-Oxford
+Michigan Technological University
+Middle Georgia State University
+Middle Tennessee State University
+Midland College
+Midwestern State University
+Miller-Motte College-Chattanooga
+Millersville University of Pennsylvania
+Minnesota State University Moorhead
+Minnesota State University-Mankato
+MiraCosta College
+Mississippi State University
+Missouri Baptist University
+Missouri Southern State University
+Missouri State University-Springfield
+Missouri University of Science and Technology
+Missouri Western State University
+Modesto Junior College
+Molloy University
+Monmouth University
+Monroe University
+Montana State University
+Montana State University Billings
+Montclair State University
+Morehead State University
+Morgan State University
+Mt Hood Community College
+Murray State University
+National Louis University
+National University
+Navarro College
+Nevada State University
+New Jersey City University
+New Jersey Institute of Technology
+New Mexico State University-Main Campus
+New York Institute of Technology
+New York University
+Nicholls State University
+Nightingale College
+Norfolk State University
+North Carolina A & T State University
+North Carolina Central University
+North Carolina State University at Raleigh
+North Dakota State University-Main Campus
+North Seattle College
+Northeastern Illinois University
+Northeastern State University
+Northeastern University
+Northern Arizona University
+Northern Illinois University
+Northern Kentucky University
+Northern Michigan University
+Northern State University
+Northland Pioneer College
+Northwest Florida State College
+Northwest Missouri State University
+Northwestern Michigan College
+Northwestern State University of Louisiana
+Northwestern University
+Nova Southeastern University
+NUC University
+Oakland University
+Odessa College
+Ohio University-Main Campus
+Oklahoma State University-Main Campus
+Oklahoma State University-Oklahoma City
+Old Dominion University
+Olympic College
+Oral Roberts University
+Oregon Institute of Technology
+Ozarks Technical Community College
+Pace University
+Palm Beach Atlantic University
+Palm Beach State College
+Paradise Valley Community College
+Park University
+Pasco-Hernando State College
+Pennsylvania College of Technology
+Pennsylvania State University-Penn State Erie-Behrend College
+Pennsylvania State University-Penn State Harrisburg
+Pennsylvania State University-World Campus
+Pennsylvania Western University
+Pensacola State College
+Pepperdine University
+Pierce College District
+Pikes Peak State College
+Pittsburg State University
+Plymouth State University
+Point Loma Nazarene University
+Polk State College
+Pontifical Catholic University of Puerto Rico-Ponce
+Portland State University
+Post University
+Prairie View A & M University
+Pratt Institute-Main
+Princeton University
+Providence College
+Pueblo Community College
+Purdue University Fort Wayne
+Purdue University Northwest
+Quincy College
+Quinnipiac University
+Radford University
+Ramapo College of New Jersey
+Rasmussen University-Florida
+Rasmussen University-Minnesota
+Red Rocks Community College
+Regent University
+Rensselaer Polytechnic Institute
+Rhode Island College
+Rice University
+Rider University
+Rio Hondo College
+Rio Salado College
+Robert Morris University
+Rochester Institute of Technology
+Roger Williams University
+Rogers State University
+Rose State College
+Rowan University
+Rutgers University-Camden
+Rutgers University-Newark
+Sacred Heart University
+Saginaw Valley State University
+Saint Cloud State University
+Saint Johns River State College
+Saint Joseph's University - Philadelphia
+Saint Leo University
+Saint Louis Community College
+Saint Louis University
+Saint Xavier University
+Salem State University
+Salisbury University
+Sam Houston State University
+Samford University
+San Antonio College
+San Diego Mesa College
+San Francisco State University
+San Jose State University
+Santa Ana College
+Santa Clara University
+Santa Fe College
+Santa Monica College
+Savannah College of Art and Design
+School of Visual Arts
+Schoolcraft Community College District
+Seattle Central College
+Seattle University
+Seminole State College of Florida
+Seton Hall University
+Shasta College
+Shepherd University
+Shippensburg University of Pennsylvania
+Siena College
+Sinclair Community College
+Skagit Valley College
+Skyline College
+Slippery Rock University of Pennsylvania
+Snow College
+Solano Community College
+Sonoma State University
+South College
+South Dakota State University
+South Seattle College
+South Texas College
+South University-Savannah Online
+Southeast Missouri State University
+Southeastern Louisiana University
+Southeastern Oklahoma State University
+Southeastern University
+Southern Arkansas University Main Campus
+Southern Connecticut State University
+Southern Illinois University Edwardsville
+Southern Illinois University-Carbondale
+Southern Methodist University
+Southern Oregon University
+Southern University and A & M College
+Southern Utah University
+Southwest Minnesota State University
+Southwestern Oklahoma State University
+Spelman College
+Spokane Community College
+Spokane Falls Community College
+St Olaf College
+St Petersburg College
+St. John's University-New York
+St. Joseph's University-New York
+St. Thomas University
+Stanford University
+State College of Florida-Manatee-Sarasota
+State University of New York at Cortland
+State University of New York at New Paltz
+State University of New York at Oswego
+State University of New York at Plattsburgh
+Stephen F Austin State University
+Stevens Institute of Technology
+Stevenson University
+Stockton University
+Stony Brook University
+Strayer University-Global Region
+SUNY Brockport
+SUNY Buffalo State University
+Syracuse University
+Tacoma Community College
+Tallahassee State College
+Tarleton State University
+Temple University
+Tennessee State University
+Tennessee Technological University
+Texas A & M International University
+Texas A & M University-Corpus Christi
+Texas A&M University-Kingsville
+Texas A&M University-San Antonio
+Texas Christian University
+Texas Southern University
+Texas Woman's University
+The College of New Jersey
+The New School
+The University of Montana
+The University of Tampa
+The University of Tennessee-Chattanooga
+The University of Tennessee-Martin
+The University of Texas at Dallas
+The University of Texas at El Paso
+The University of Texas at Tyler
+The University of Texas Rio Grande Valley
+Thomas Edison State University
+Towson University
+Trinity Valley Community College
+Troy University
+Truckee Meadows Community College
+Tufts University
+Tulane University of Louisiana
+Tyler Junior College
+Unitek College
+Unity Environmental University
+Universidad Ana G. Mendez-Gurabo Campus
+University at Albany
+University at Buffalo
+University of Akron Main Campus
+University of Alabama at Birmingham
+University of Alabama in Huntsville
+University of Alaska Anchorage
+University of Alaska Fairbanks
+University of Arkansas
+University of Arkansas at Little Rock
+University of Arkansas-Fort Smith
+University of California-Merced
+University of California-Riverside
+University of California-Santa Barbara
+University of California-Santa Cruz
+University of Central Arkansas
+University of Central Missouri
+University of Central Oklahoma
+University of Chicago
+University of Cincinnati-Blue Ash College
+University of Cincinnati-Clermont College
+University of Colorado Colorado Springs
+University of Colorado Denver/Anschutz Medical Campus
+University of Connecticut
+University of Dayton
+University of Delaware
+University of Denver
+University of Florida-Online
+University of Hawaii at Manoa
+University of Houston-Clear Lake
+University of Houston-Downtown
+University of Idaho
+University of Illinois Chicago
+University of Iowa
+University of Kansas
+University of Kentucky
+University of Louisiana at Lafayette
+University of Louisiana at Monroe
+University of Louisville
+University of Maine
+University of Maryland-Baltimore County
+University of Massachusetts Global
+University of Massachusetts-Amherst
+University of Massachusetts-Boston
+University of Massachusetts-Dartmouth
+University of Massachusetts-Lowell
+University of Memphis
+University of Miami
+University of Michigan-Dearborn
+University of Michigan-Flint
+University of Minnesota-Duluth
+University of Mississippi
+University of Missouri-Columbia
+University of Missouri-Kansas City
+University of Missouri-St Louis
+University of Nebraska at Omaha
+University of Nebraska-Lincoln
+University of Nevada-Las Vegas
+University of Nevada-Reno
+University of New Hampshire-Main Campus
+University of New Mexico-Main Campus
+University of New Orleans
+University of North Alabama
+University of North Carolina at Chapel Hill
+University of North Carolina at Charlotte
+University of North Carolina at Greensboro
+University of North Carolina at Pembroke
+University of North Carolina Wilmington
+University of North Dakota
+University of North Florida
+University of North Georgia
+University of Northern Colorado
+University of Northern Iowa
+University of Notre Dame
+University of Oklahoma-Norman Campus
+University of Oregon
+University of Pennsylvania
+University of Pittsburgh-Pittsburgh Campus
+University of Puerto Rico-Mayaguez
+University of Puerto Rico-Rio Piedras
+University of Rhode Island
+University of Rochester
+University of San Diego
+University of San Francisco
+University of South Alabama
+University of South Dakota
+University of Southern California
+University of Southern Indiana
+University of Southern Maine
+University of Southern Mississippi
+University of St Thomas
+University of the Cumberlands
+University of Toledo
+University of Utah
+University of Vermont
+University of Virginia-Main Campus
+University of Washington-Bothell Campus
+University of West Florida
+University of West Georgia
+University of Wisconsin-Eau Claire
+University of Wisconsin-Green Bay
+University of Wisconsin-La Crosse
+University of Wisconsin-Milwaukee
+University of Wisconsin-Oshkosh
+University of Wisconsin-Platteville
+University of Wisconsin-Stevens Point
+University of Wisconsin-Stout
+University of Wisconsin-Whitewater
+University of Wyoming
+Utah State University
+Utah Tech University
+Valdosta State University
+Vanderbilt University
+Villanova University
+Vincennes University
+Virginia Commonwealth University
+Virginia State University
+Wake Forest University
+Walden University
+Washburn University
+`;
+
+export const schoolList = schools
+  .trim()
+  .split("\n")
+  .map(s => s.trim())
+  .filter(Boolean)
+  .sort((a, b) => a.localeCompare(b));
